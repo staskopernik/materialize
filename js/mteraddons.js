0680aec265324c77c913ea575013aaafe8159c76
Mykhailo <staskopernik@gmail.com>
2017-11-11 20:50:10 +0000

    add parallax container

--- js/mteraddons.js
+++ js/mteraddons.js
@@ -1,5 +1,6 @@
 $( document ).ready(function () {
       $(".button-collapse").sideNav();
+      $(".parallax").parallax();
       $(".carousel.carousel-slider").carousel({
             fullWidth: true,
             indicators: true,
