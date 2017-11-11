7495b81cff9ce31405b14c56f34249ee06abb8cd
Mykhailo <staskopernik@gmail.com>
2017-11-11 20:06:50 +0000

    add slider

--- js/mteraddons.js
+++ js/mteraddons.js
@@ -1,7 +1,9 @@
 $( document ).ready(function () {
       $(".button-collapse").sideNav();
       $(".carousel.carousel-slider").carousel({
-            fullWidth:true,
-            indicators:true,
+            fullWidth: true,
+            indicators: true,
+            duration: 500,
+            noWrap:true,
       })
 })
