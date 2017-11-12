ec9a00e5c03f48ef19d96d40c8728f6e2e8aef9e
Mykhailo <staskopernik@gmail.com>
2017-11-12 17:39:39 +0000

    update modal window

--- js/mteraddons.js
+++ js/mteraddons.js
@@ -1,6 +1,14 @@
 $( document ).ready(function () {
       $(".button-collapse").sideNav();
       $(".parallax").parallax();
+      $(".modal").modal({
+            dismissible:false,
+            opacity:.9,
+            inDuration: 300,
+            outDuration: 300,
+            startingtop:"70%",
+            
+      });
       $(".carousel.carousel-slider").carousel({
             fullWidth: true,
             indicators: true,
